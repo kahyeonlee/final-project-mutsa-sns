475488e0754d64e24b41839048ed338a9c263f0c
gahyun <gmeoqq@gmail.com>
2022-12-22 10:51:02 +0900

    fix : login 기능
    1. UserDto 추가

--- src/main/java/com/mutsasns/domain/dto/UserDto.java
+++ src/main/java/com/mutsasns/domain/dto/UserDto.java
@@ -1,6 +1,7 @@
 package com.mutsasns.domain.dto;
 
 import com.mutsasns.domain.User;
+import com.mutsasns.domain.UserRole;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,5 +16,7 @@ public class UserDto {
     private Long id;
     private String userName;
     private String password;
+    private UserRole userRole;
+
 
 }
