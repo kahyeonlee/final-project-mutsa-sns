26c6b31f10bced0de3ba99abb3428b201e20f92b
gahyun <gmeoqq@gmail.com>
2022-12-24 23:40:00 +0900

    refactor : 수정

--- src/main/java/com/mutsasns/configuration/DateApplication.java
+++ src/main/java/com/mutsasns/configuration/DateApplication.java
@@ -1,12 +1,11 @@
 package com.mutsasns.configuration;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class Application {
+public class DateApplication {
 
     public static void main(String[] args) {
     }
