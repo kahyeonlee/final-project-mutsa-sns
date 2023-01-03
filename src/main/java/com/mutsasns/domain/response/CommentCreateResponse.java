53026bce8e6d60c767906a71188a707b9c8c04ea
gahyun <gmeoqq@gmail.com>
2023-01-04 02:06:13 +0900

    [feat] : 댓글 작성 기능 추가

---
+++ src/main/java/com/mutsasns/domain/response/CommentCreateResponse.java
@@ -0,0 +1,35 @@
+package com.mutsasns.domain.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.mutsasns.domain.entity.Comment;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+public class CommentCreateResponse {
+    private Long id;
+    private String comment;
+    private String userName;
+    private Long postId;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime createdAt;
+
+
+    public static CommentCreateResponse of(Comment comment) {
+        return CommentCreateResponse.builder()
+                .id(comment.getId())
+                .comment(comment.getComment())
+                .userName(comment.getUser().getUserName())
+                .postId(comment.getPost().getId())
+                .createdAt(comment.getCreatedAt())
+                .build();
+    }
+}
