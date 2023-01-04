28b41aec63d72034e13111785380549fe88a7ba0
gahyun <gmeoqq@gmail.com>
2023-01-04 14:59:28 +0900

    [test] : 댓글 수정 test
    1. 수정 성공
    2. 수정 실패
    - 인증실패
    - post가 존재하지 않음
    - 작성자 불일치
    - DB에러

--- src/test/java/com/mutsasns/controller/CommentControllerTest.java
+++ src/test/java/com/mutsasns/controller/CommentControllerTest.java
@@ -1,8 +1,8 @@
 package com.mutsasns.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mutsasns.domain.dto.CommentCreateRequest;
-import com.mutsasns.domain.response.CommentCreateResponse;
+import com.mutsasns.domain.dto.CommentRequest;
+import com.mutsasns.domain.response.CommentResponse;
 import com.mutsasns.exception.AppException;
 import com.mutsasns.exception.ErrorCode;
 import com.mutsasns.service.CommentService;
@@ -44,12 +44,12 @@ class createComment {
     @WithMockUser
     void comment_success() throws Exception {
         //given
-        CommentCreateRequest request = CommentCreateRequest.builder()
+        CommentRequest request = CommentRequest.builder()
                 .comment("mockComment")
                 .build();
 
         //when
-        when(commentService.createComment(any(), any(), any())).thenReturn(mock(CommentCreateResponse.class));
+        when(commentService.createComment(any(), any(), any())).thenReturn(mock(CommentResponse.class));
 
         //then
         mockMvc.perform(post("/api/v1/posts/1/comments")
@@ -70,7 +70,7 @@ class createComment {
     @DisplayName("댓글 실패 - 로그인 하지 않은 경우")
     void comment_fail() throws Exception {
         //given
-        CommentCreateRequest request = CommentCreateRequest.builder()
+        CommentRequest request = CommentRequest.builder()
                 .comment("mockComment")
                 .build();
 
@@ -90,7 +90,7 @@ class createComment {
     @DisplayName("댓글 성공 - 게시물이 존재하지 않은 경우")
     void comment_fail2() throws Exception {
         //given
-        CommentCreateRequest request = CommentCreateRequest.builder()
+        CommentRequest request = CommentRequest.builder()
                 .comment("mockComment")
                 .build();
 
@@ -106,4 +106,118 @@ class createComment {
                 .andExpect(status().isUnauthorized());
     }
 }
+
+    @Nested
+    @DisplayName("댓글수정")
+    class ModifyComment {
+        @Test
+        @DisplayName("댓글 수정 성공")
+        @WithMockUser
+        void comment_success() throws Exception {
+            //given
+            CommentRequest request = CommentRequest.builder()
+                    .comment("mockComment")
+                    .build();
+
+            //when
+            when(commentService.modifyComment(any(),any(), any(), any())).thenReturn(mock(CommentResponse.class));
+
+            //then
+            mockMvc.perform(post("/api/v1/posts/1/comments/1")
+                            .with(csrf())
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsBytes(request)))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.resultCode").exists())
+                    .andExpect(jsonPath("$.result.id").exists())
+                    .andExpect(jsonPath("$.result.comment").exists())
+                    .andExpect(jsonPath("$.result.userName").exists())
+                    .andExpect(jsonPath("$.result.postId").exists())
+                    .andExpect(jsonPath("$.result.createdAt").exists())
+                    .andExpect(jsonPath("$.result.lastModifiedAt").exists());
+        }
+
+        @Test
+        @DisplayName("댓글 수정 실패 - 인증실패")
+        void comment_fail() throws Exception {
+            //given
+            CommentRequest request = CommentRequest.builder()
+                    .comment("mockComment")
+                    .build();
+
+            //when
+            when(commentService.modifyComment(any(),any(), any(), any())).thenThrow(new AppException(ErrorCode.INVALID_TOKEN));
+
+            //then
+            mockMvc.perform(post("/api/v1/posts/1/comments/1")
+                            .with(csrf())
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsBytes(request)))
+                    .andDo(print())
+                    .andExpect(status().isUnauthorized());
+        }
+
+        @Test
+        @DisplayName("댓글 수정 실패 - Post 없는 경우")
+        void comment_fail2() throws Exception {
+            //given
+            CommentRequest request = CommentRequest.builder()
+                    .comment("mockComment")
+                    .build();
+
+            //when
+            when(commentService.modifyComment(any(),any(), any(), any())).thenThrow(new AppException(ErrorCode.POST_NOT_FOUND));
+
+            //then
+            mockMvc.perform(post("/api/v1/posts/1/comments/1")
+                            .with(csrf())
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsBytes(request)))
+                    .andDo(print())
+                    .andExpect(status().isNotFound());
+        }
+
+        @Test
+        @DisplayName("댓글 수정 실패 - 작성자 불일치")
+        void comment_fail3() throws Exception {
+            //given
+            CommentRequest request = CommentRequest.builder()
+                    .comment("mockComment")
+                    .build();
+
+            //when
+            when(commentService.modifyComment(any(),any(), any(), any())).thenThrow(new AppException(ErrorCode.INVALID_PERMISSION));
+
+            //then
+            mockMvc.perform(post("/api/v1/posts/1/comments/1")
+                            .with(csrf())
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsBytes(request)))
+                    .andDo(print())
+                    .andExpect(status().isUnauthorized());
+        }
+        @Test
+        @DisplayName("댓글 수정 실패 - DB에러")
+        void comment_fail4() throws Exception {
+            //given
+            CommentRequest request = CommentRequest.builder()
+                    .comment("mockComment")
+                    .build();
+
+            //when
+            when(commentService.modifyComment(any(),any(), any(), any())).thenThrow(new AppException(ErrorCode.DATABASE_ERROR));
+
+            //then
+            mockMvc.perform(post("/api/v1/posts/1/comments/1")
+                            .with(csrf())
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsBytes(request)))
+                    .andDo(print())
+                    .andExpect(status().is(ErrorCode.DATABASE_ERROR.getHttpStatus().value()));
+
+        }
+
+    }
+
 }
